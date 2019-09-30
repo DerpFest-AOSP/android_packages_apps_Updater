f7154adec99681d173e9eaca978a47ae486cb5db
NurKeinNeid <nurkeinneid@derpfest.org>
2019-09-30 14:04:01 +0300

    Updater: Remove incremental build version prop stuff
    * we aren't using that
    
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- app/src/main/java/org/lineageos/updater/deviceinfo/DeviceInfoUtils.kt
+++ app/src/main/java/org/lineageos/updater/deviceinfo/DeviceInfoUtils.kt
@@ -15,7 +15,6 @@ object DeviceInfoUtils : SettingsLibDeviceInfoUtils() {
     private const val PROP_ALLOW_MAJOR_UPGRADES = "derpfest.updater.allow_major_upgrades"
     private const val PROP_BUILD_DATE = "ro.build.date.utc"
     private const val PROP_BUILD_VERSION = "ro.derpfest.version"
-    private const val PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental"
     private const val PROP_DEVICE = "ro.derpfest.device"
     private const val PROP_NEXT_DEVICE = "ro.updater.next_device"
     private const val PROP_RELEASE_TYPE = "ro.derpfest.releasetype"
@@ -33,9 +32,6 @@ object DeviceInfoUtils : SettingsLibDeviceInfoUtils() {
 
     @JvmStatic
     val buildVersion: String = SystemProperties.get(PROP_BUILD_VERSION, "")
-
-    @JvmStatic
-    val buildVersionIncremental: String = SystemProperties.get(PROP_BUILD_VERSION_INCREMENTAL, "")
 
     @JvmStatic
     val device: String = SystemProperties.get(PROP_NEXT_DEVICE, SystemProperties.get(PROP_DEVICE))
